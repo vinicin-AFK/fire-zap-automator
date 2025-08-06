c4532846af45c7e9e0311c0948ad8f7cf6a03cf5
vinicin-AFK <vv9250400@gmail.com>
2025-08-06 20:39:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,30 +8,23 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(cors());
-
 let isBotRunning = false;
+
+app.use(cors());
 
 app.get("/start-bot", (req, res) => {
   if (isBotRunning) return res.send("Bot já está rodando!");
-
-  const subprocess = exec("node robo.js", (err, stdout, stderr) => {
-    if (err) {
-      console.error("Erro ao iniciar bot:", err);
-      return res.status(500).send("Erro ao iniciar bot.");
-    }
-  });
-
+  exec("node robo.js");
   isBotRunning = true;
   res.send("Bot iniciado com sucesso.");
 });
 
-io.on("connection", (socket) => {
-  console.log("Frontend conectado.");
+io.on("connection", () => {
+  console.log("Frontend conectado via socket");
 });
 
 server.listen(3000, () => {
-  console.log("🔥 Servidor backend iniciado na porta 3000");
+  console.log("Servidor local rodando em http://localhost:3000");
 });
 
 module.exports = io;
